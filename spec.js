f4ad94b4b91dc1b8e4bdfd1912bc18fb81354f39
ShaunWard <shaunalwynward@googlemail.com>
2020-08-18 10:54:55 +0000

    JSON and AJAX revision

---
+++ spec.js
@@ -0,0 +1,29 @@
+let myCat = {
+    "name": "Edmund",
+    "species": "cat",
+    "favFood": "tuna"
+}
+
+myCat.name
+// Returns Edmund - this is how to access object info
+
+let myFavColors = ["blue", "green", "purple"];
+
+myFavColors[2]
+// Returns purple - this is how to access array info
+
+let thePets = [
+    {
+    "name": "Edmund",
+    "species": "cat",
+    "favFood": "tuna"
+    },
+    {
+    "name": "Barky",
+    "species": "Dog",
+    "favFood": "Carrots"
+    }
+]
+
+thePets[1].favFood
+// Returns Carrots - this is how to access objects in an array, this is JSON!
