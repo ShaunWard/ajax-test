52dc976978d98442d739b5ee09228eb9c0a4f4a7
ShaunWard <shaunalwynward@googlemail.com>
2020-08-18 16:49:05 +0000

    Testing out .getJSON

--- main-ajax.js
+++ main-ajax.js
@@ -1,10 +1,26 @@
 let btn = document.getElementById("btn");
+let clr = document.getElementById("clr");
+let activities;
 
 btn.addEventListener('click', myRequest);
+clr.addEventListener('click', myClear);
 
 function myRequest(){
-    $.getJSON("https://ci-swapi.herokuapp.com/api/", function(swResults){
+    document.getElementById("info").innerHTML = "";
+    $.getJSON("https://www.boredapi.com/api/activity/", function(swResults){
         console.log(swResults);
+        let things =    `<div>stuff within a div on a page</div>
+                        <div>and here's an activity: ${swResults.activity}</div>
+                        <div>with this many participants: ${swResults.participants}</div>
+                        <div>and how much does all this cost you ask?</div>
+                        <div><strong>£${swResults.price}</strong></div>`
+        $("#info").html(things)
+    
     });
+    
+}
+
+function myClear() {
+    document.getElementById("info").innerHTML = "";
 }
 
